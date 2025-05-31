1305750d8751ac7cd35aadd6ae182ec292f68797
wolf-yuan <wolf@wolf-yuan.dev>
2025-05-31 20:15:34 +0800

    feat: add breathing hospital data

--- Taipei-City-Dashboard-FE/src/store/mapStore.js
+++ Taipei-City-Dashboard-FE/src/store/mapStore.js
@@ -195,7 +195,8 @@ export const useMapStore = defineStore("map", {
 				"bike_orange",
 				"bike_red",
 				"cctv",
-				"aed"
+				"aed",
+				"lung"
 			];
 			images.forEach((element) => {
 				this.map.loadImage(
